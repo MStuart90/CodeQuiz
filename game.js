d65e3d64fe6179c7d5a7fff069cb6c08e3d48351
Melanie Stuart <mmooradian90@yahoo.com>
2020-09-25 23:00:11 -0400

    Fixed initial index.html doc for small spelling errors and updated coresponding CSS.

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
-const question =document.querySelector('#question');
-const choices =Array.from(document.querySelectorALL('.choice-text'));
-const progressText =document.querySelector('#progressText');
-const scoreText =document.querySelector('#score');
-const progressBarFull =document.querySelector('#progressBarFull');
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorALL('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
-let acceptingAnswers = truelet score =0
+let acceptingAnswers = true
+let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
-
-    {
+     {
        question: 'what is 2 + 2?', 
        choice1: '2',
        choice2: '4',
@@ -112,9 +112,9 @@ let questions = [
 ]
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 10
+const MAX_QUESTIONS = 4
 
-startGame = ()=> {
+startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
@@ -122,14 +122,15 @@ startGame = ()=> {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore, score')
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
         return window.location.assign('/end.html')
     }
 
     questionCounter++
-    progressText.innerText = 'question ${questionCounter} of ${MAX_QUESTIONS}'
-    progressBarFull.getElementsByClassName.width = '${(questionCounter/MAX_QUESTIONS) * 100 }%'
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+    progressBarFull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
@@ -165,6 +166,15 @@ choices.forEach(choice => {
             setTimeout(() => {
                 selectedChoice.parentElement.classList.remove(classToApply)
                 getNewQuestion()
-            }
+
+            }, 1000)
     })
 })
+
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = Score
+}
+
+startGame()
