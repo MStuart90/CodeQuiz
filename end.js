8c126505e3c4ad57c2536fddfeb13a541d5ff28c
Melanie Stuart <mmooradian90@yahoo.com>
2020-09-26 09:22:31 -0400

    Completed most of the JS for the end portion of the game.

--- end.js
+++ end.js
@@ -1,4 +1,35 @@
 const usernamer = document.querySelector('username')
-const usernamer = document.querySelector('username')
-const usernamer = document.querySelector('username')
-const usernamer = document.querySelector('username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = document.querySelector('#mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+usernamer.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.prventDefault()
+
+    const score ={
+        score:mostRecentScore,
+        name: username.value
+    }
+}
+
+highScores.push(score)
+
+highScores.sort((a,b) => {
+    return b.score -a.score
+})
+
+
+highScores.splice(5)
+
+localStorage.setItem('highScores', JSON.stringify(highScores))
+window.location.assign('/')
