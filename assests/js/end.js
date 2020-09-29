b85ec4a1acb03d91db0682ad7449d71ef2896ce8
Melanie Stuart <mmooradian90@yahoo.com>
2020-09-29 14:11:00 -0400

    fixing end.js bug.

--- assests/js/end.js
+++ assests/js/end.js
@@ -29,5 +29,5 @@ highScores.sort((a,b) => b.score - a.score)
 highScores.splice(5)
 
 localStorage.setItem('highScores', JSON.stringify(highScores))
-window.location.assign('/')
+window.location.assign('./index.html')
 }
